02964abb0a9602c1c58984ad6708fe7b0e3240ff
geothomas <geothomas@cet.ac.in>
2020-09-18 11:04:20 +0530

    first commit

---
+++ app/src/main/java/com/example/btcdiarie/Main2Activity.java
@@ -0,0 +1,31 @@
+package com.example.btcdiarie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Main2Activity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Button button = findViewById(R.id.button);
+   button.setOnClickListener(new View.OnClickListener() {
+       @Override
+       public void onClick(View v) {
+           openActivity2();
+       }
+   });
+    }
+    public void openActivity2() {
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
+    }
+
+}
